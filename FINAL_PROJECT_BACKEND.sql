f933fdcdb58a7024ed277f6f5c158bba0faf176b
ctoledo03 <144138050+ctoledo03@users.noreply.github.com>
2023-12-05 11:16:25 -0500

    submitted files

--- FINAL_PROJECT_BACKEND.sql
+++ FINAL_PROJECT_BACKEND.sql
@@ -1,10 +1,9 @@
+-- Run first before doing anything in each session
 SET SERVEROUTPUT ON;
-
 SELECT R_BOOKID_SEQ.NEXTVAL FROM DUAL;
 SELECT B_BOOKID_SEQ.NEXTVAL FROM DUAL;
 
 -- USER PACKAGE STARTS HERE
-
 CREATE OR REPLACE PACKAGE USERPACKAGE AS
     FUNCTION SEARCHBOOK(BOOK_ID NUMBER) RETURN VARCHAR2;
     PROCEDURE BORROWBOOK (BOOK_ID NUMBER, USER_ID NUMBER);
@@ -13,16 +12,17 @@ CREATE OR REPLACE PACKAGE USERPACKAGE AS
 END USERPACKAGE;
 
 CREATE OR REPLACE PACKAGE BODY USERPACKAGE AS 
+ 
     FUNCTION SEARCHBOOK(BOOK_ID NUMBER) RETURN VARCHAR2 IS
         AVAILABLE NUMBER(2);
         BOOK_TITLE VARCHAR2(30);
         BOOK_LOCATION VARCHAR2(6);
     BEGIN
-        -- Using an alias for better clarity
         SELECT BOOK_COUNT INTO AVAILABLE FROM LIBRARY_BOOKS LB WHERE LB.BOOK_ID = SEARCHBOOK.BOOK_ID;
         SELECT BOOK_NAME INTO BOOK_TITLE FROM LIBRARY_BOOKS LB WHERE LB.BOOK_ID = SEARCHBOOK.BOOK_ID;
         SELECT BOOK_LOC INTO BOOK_LOCATION FROM LIBRARY_BOOKS LB WHERE LB.BOOK_ID = SEARCHBOOK.BOOK_ID;
         
+        -- If there's no book that has the id that the user gave
         IF AVAILABLE = 0 THEN
             RETURN 'Book ID not found.';
         ELSE
@@ -33,26 +33,28 @@ CREATE OR REPLACE PACKAGE BODY USERPACKAGE AS
     ----------------------------------------------------------------------------------------------------------------
     
     PROCEDURE BORROWBOOK (BOOK_ID NUMBER, USER_ID NUMBER) IS 
-        B_BOOK_SEQ NUMBER;
-        BOOK_NAME VARCHAR2(30);
-        IS_RESERVED VARCHAR2(5);
-        COPY_COUNT NUMBER;
-        USER_BORROWED VARCHAR2(5) := 'FALSE';
-        ROWCOUNT NUMBER;
-        BORROWED VARCHAR2(5) := 'FALSE';
-        RESERVED_COUNT NUMBER;
-        PRIORITY_ID NUMBER; -- ID OF THE FIRST PERSON TO RESERVE (THE ONE WITH THE LOWEST R_BOOK_ID)
+        B_BOOK_SEQ NUMBER; -- Checks the current number of the sequence B_BOOK_ID
+        BOOK_NAME VARCHAR2(30); -- Book name
+        IS_RESERVED VARCHAR2(5); -- False if the book is not reserved
+        COPY_COUNT NUMBER; -- The count of available book copies
+        USER_BORROWED VARCHAR2(5) := 'FALSE'; -- False if the user already borrowed the book they're trying to borrow
+        BORROWED VARCHAR2(5) := 'FALSE'; -- False if the book is successfully borrowed
+        RESERVED_COUNT NUMBER; -- Number of copies of the book that is reserved in the reserved_book table
+        PRIORITY_ID NUMBER; -- ID of the first person to reserve (the one with the lowest r_book_id)
     BEGIN
         SELECT B_BOOKID_SEQ.CURRVAL INTO B_BOOK_SEQ FROM DUAL;
         SELECT BOOK_NAME, RESERVED, BOOK_COUNT INTO BOOK_NAME, IS_RESERVED, COPY_COUNT FROM LIBRARY_BOOKS WHERE LIBRARY_BOOKS.BOOK_ID = BORROWBOOK.BOOK_ID;
         
+        -- Loop checks if the BOOK_ID given is already borrowed by the user
         FOR REC IN (SELECT BOOK_ID, BORROWER_ID FROM BORROWED_BOOKS) LOOP
             IF REC.BOOK_ID = BOOK_ID AND REC.BORROWER_ID = USER_ID THEN
                 USER_BORROWED := 'TRUE';
             END IF;
         END LOOP;
         
+        -- If it's not reserved or there are more than 1 copies then continue
         IF IS_RESERVED = 'FALSE' OR COPY_COUNT > 1  THEN
+            -- If the copy count is not 0 and the user hasn't borrowed the book he's trying to borrow yet, then let the user borrow the book
             IF COPY_COUNT <> 0 AND USER_BORROWED = 'FALSE' THEN
                 UPDATE LIBRARY_BOOKS SET BOOK_COUNT = BOOK_COUNT - 1 WHERE LIBRARY_BOOKS.BOOK_ID = BORROWBOOK.BOOK_ID;
                 UPDATE BORROWED_BOOKS SET BORROWER_ID = USER_ID WHERE B_BOOK_ID = (B_BOOK_SEQ + 1);
@@ -60,7 +62,9 @@ CREATE OR REPLACE PACKAGE BODY USERPACKAGE AS
                 BORROWED := 'TRUE';
             END IF;
         ELSE -- IF IT IS RESERVED AND THERE'S ONLY 1 COPY LEFT, CHECK IF THE BORROWER IS THE SAME PERSON WHO RESERVED THE BOOK
+            -- If the copy count is not 0 and the user hasn't borrowed the book he's trying to borrow yet, then let the user borrow the book
             IF COPY_COUNT <> 0 AND USER_BORROWED = 'FALSE' THEN
+                -- Select the first reserver of the book to be prioritized and notified if the book has been returned
                 SELECT RESERVER_ID INTO PRIORITY_ID
                     FROM (
                         SELECT RESERVER_ID, R_BOOK_ID
@@ -68,8 +72,9 @@ CREATE OR REPLACE PACKAGE BODY USERPACKAGE AS
                         WHERE BOOK_ID = BORROWBOOK.BOOK_ID
                         ORDER BY R_BOOK_ID
                         FETCH FIRST 1 ROW ONLY);
-            
+                
                 FOR REC IN (SELECT R_BOOK_ID, BOOK_ID, RESERVER_ID FROM RESERVED_BOOKS) LOOP
+                    -- If the user trying to borrow is the one that has the priority, then allow them to borrow the book
                     IF REC.BOOK_ID = BOOK_ID AND REC.RESERVER_ID = USER_ID AND REC.RESERVER_ID = PRIORITY_ID THEN -- IF THE BORROWER IS ALSO THE RESERVER
                         -- DO AN IF HERE TO CHECK IF THE LOWEST R_BOOK_ID IS THE ID OF THE PERSON BORROWING
                         UPDATE LIBRARY_BOOKS SET BOOK_COUNT = BOOK_COUNT - 1 WHERE LIBRARY_BOOKS.BOOK_ID = BORROWBOOK.BOOK_ID;
@@ -81,12 +86,14 @@ CREATE OR REPLACE PACKAGE BODY USERPACKAGE AS
                 END LOOP;
                 
                 SELECT COUNT(*) INTO RESERVED_COUNT FROM RESERVED_BOOKS WHERE RESERVED_BOOKS.BOOK_NAME = BORROWBOOK.BOOK_NAME;
+                -- If the book is not reserved by anyone else
                 IF RESERVED_COUNT = 0 THEN
                     UPDATE LIBRARY_BOOKS SET RESERVED = 'FALSE' WHERE LIBRARY_BOOKS.BOOK_ID = BORROWBOOK.BOOK_ID;
                 END IF;
             END IF;
         END IF;
         
+        -- Change output depending on the state of USER_BORROWED
         IF USER_BORROWED = 'TRUE' THEN
             DBMS_OUTPUT.PUT_LINE('You have already borrowed this book.');
         ELSIF BORROWED = 'FALSE' THEN
@@ -108,6 +115,7 @@ CREATE OR REPLACE PACKAGE BODY USERPACKAGE AS
         SELECT COUNT(*) INTO RECORD_COUNT FROM BORROWED_BOOKS WHERE BORROWED_BOOKS.BOOK_ID = BOOK_ID AND BORROWED_BOOKS.BORROWER_ID = USER_ID;
         SELECT COUNT(*) INTO TITLE_COUNT FROM BORROWED_BOOKS WHERE BORROWED_BOOKS.BOOK_ID = BOOK_ID AND BORROWED_BOOKS.BORROWER_ID = USER_ID;
         
+        -- Checks if the user borrowed the book that they're trying to return
         IF RECORD_COUNT <> 0 THEN
             UPDATE LIBRARY_BOOKS SET BOOK_COUNT = BOOK_COUNT + 1 WHERE LIBRARY_BOOKS.BOOK_ID = BOOK_ID;
             DELETE FROM BORROWED_BOOKS WHERE BORROWED_BOOKS.BOOK_ID = BOOK_ID AND BORROWED_BOOKS.BORROWER_ID = USER_ID;
@@ -129,8 +137,10 @@ CREATE OR REPLACE PACKAGE BODY USERPACKAGE AS
     BEGIN
         SELECT R_BOOKID_SEQ.CURRVAL INTO R_BOOK_SEQ FROM DUAL;
         SELECT BOOK_NAME INTO BOOK_NAME FROM LIBRARY_BOOKS WHERE LIBRARY_BOOKS.BOOK_ID = RESERVEBOOK.BOOK_ID;
-
+        
+        -- Triggers the RESERVE_BOOK_TRIGGER
         UPDATE LIBRARY_BOOKS SET RESERVED = 'TRUE' WHERE LIBRARY_BOOKS.BOOK_ID = RESERVEBOOK.BOOK_ID;
+        -- Sets the RESERVER_ID to USER_ID
         UPDATE RESERVED_BOOKS SET RESERVER_ID = USER_ID WHERE R_BOOK_ID = (R_BOOK_SEQ + 1);
         
         DBMS_OUTPUT.PUT_LINE('You have successfully reserved the book ' || BOOK_NAME || '. Your Reserving Book ID is ' || (R_BOOK_SEQ + 1));
@@ -148,6 +158,7 @@ FOR EACH ROW WHEN (NEW.BOOK_COUNT < OLD.BOOK_COUNT)
         BOOK_ID NUMBER(6);
         BOOK_NAME VARCHAR2(30);
     BEGIN
+        -- Retrieve user and book details from the updated row
         BOOK_ID := :NEW.BOOK_ID;
         BOOK_NAME := :NEW.BOOK_NAME;
         
@@ -159,7 +170,6 @@ END;
 CREATE OR REPLACE TRIGGER RESERVE_BOOK_TRIGGER BEFORE UPDATE OF RESERVED ON LIBRARY_BOOKS 
 FOR EACH ROW WHEN (NEW.RESERVED = 'TRUE')
     DECLARE
-        -- Declare variables to store relevant information
         BOOK_ID NUMBER(6);
         BOOK_NAME VARCHAR2(30);
     BEGIN
@@ -167,38 +177,37 @@ FOR EACH ROW WHEN (NEW.RESERVED = 'TRUE')
         BOOK_ID := :NEW.BOOK_ID;
         BOOK_NAME := :NEW.BOOK_NAME;
         
-        -- Insert a record into BORROWED_BOOKS table
         INSERT INTO RESERVED_BOOKS (BOOK_ID, BOOK_NAME) VALUES (BOOK_ID, BOOK_NAME);
 END;
 /
 
--- SEARCH BOOK (WORKING)
+-- SEARCH BOOK
 DECLARE
-    IS_AVAILABLE VARCHAR2(50);
+    IS_AVAILABLE VARCHAR2(60);
 BEGIN
-    IS_AVAILABLE := USERPACKAGE.SEARCHBOOK(1056);
+    IS_AVAILABLE := USERPACKAGE.SEARCHBOOK(1055);
     DBMS_OUTPUT.PUT_LINE(IS_AVAILABLE);
 END;
 
--- BORROW BOOK (WORKING)
+-- BORROW BOOK 
 BEGIN
-    USERPACKAGE.BORROWBOOK(1056, 2);
+    USERPACKAGE.BORROWBOOK(1055, 2);
 END;
 
--- RETURN BOOK (WORKING)
+-- RETURN BOOK 
 BEGIN
-    USERPACKAGE.RETURNBOOK(1056, 2);
+    USERPACKAGE.RETURNBOOK(1055, 5);
 END;
 
--- RESERVE BOOK (WORKING)
+-- RESERVE BOOK
 BEGIN
-    USERPACKAGE.RESERVEBOOK(1055, 2);
+    USERPACKAGE.RESERVEBOOK(1055, 3);
 END;
 
--- USER PACKAGE ENDS HERE
+-- USER PACKAGE ENDS HERE -------------------------------------------------------------------------------------------
 
 
--- STAFF PACKAGE STARTS HERE
+-- STAFF PACKAGE STARTS HERE ----------------------------------------------------------------------------------------
 
 CREATE OR REPLACE PACKAGE STAFFPACKAGE AS
     PROCEDURE ADDBOOK(TITLE VARCHAR2, LOCATION VARCHAR2, COUNT NUMBER);
@@ -215,12 +224,12 @@ CREATE OR REPLACE PACKAGE BODY STAFFPACKAGE AS
         -- Check if the book already exists
         SELECT BOOK_ID, BOOK_COUNT INTO BOOK_ID, BOOK_COUNT FROM LIBRARY_BOOKS WHERE BOOK_NAME = TITLE AND BOOK_LOC = LOCATION;
 
-        -- Book exists, update the count
-        UPDATE LIBRARY_BOOKS SET BOOK_COUNT = BOOK_COUNT + COUNT WHERE BOOK_ID = BOOK_ID;
+        -- If book exists then update the count
+        UPDATE LIBRARY_BOOKS SET BOOK_COUNT = BOOK_COUNT + COUNT WHERE LIBRARY_BOOKS.BOOK_ID = ADDBOOK.BOOK_ID;
         DBMS_OUTPUT.PUT_LINE('Book count successfully updated');
 
 
-        -- Book doesn't exist, insert a new record
+        -- If book doesn't exist then insert a new record
         EXCEPTION
             WHEN NO_DATA_FOUND THEN
                 INSERT INTO LIBRARY_BOOKS (BOOK_NAME, BOOK_LOC, BOOK_COUNT) VALUES (TITLE, LOCATION, COUNT);
@@ -262,7 +271,6 @@ CREATE OR REPLACE PACKAGE BODY STAFFPACKAGE AS
     BEGIN
     -- Generate report based on the specified type
         IF REPORT_TYPE = 'BORROWED' THEN
-            -- Open the borrowed cursor
             OPEN BORROWED_CURSOR;
             
             -- Fetch and print records
@@ -271,12 +279,10 @@ CREATE OR REPLACE PACKAGE BODY STAFFPACKAGE AS
                 EXIT WHEN BORROWED_CURSOR%NOTFOUND;
                 DBMS_OUTPUT.PUT_LINE('Book ID: ' || BORROWED_ID || ', Book Name: ' || BORROWED_NAME || ', User ID: ' || BORROWER_ID);
             END LOOP;
-            
-            -- Close the borrowed cursor
+
             CLOSE BORROWED_CURSOR;
             
         ELSIF REPORT_TYPE = 'RESERVED' THEN
-            -- Open the reserved cursor
             OPEN RESERVED_CURSOR;
             
             -- Fetch and print records
@@ -286,7 +292,6 @@ CREATE OR REPLACE PACKAGE BODY STAFFPACKAGE AS
                 DBMS_OUTPUT.PUT_LINE('Book ID: ' || RESERVED_ID || ', Book Name: ' || RESERVED_NAME || ', User ID: ' || RESERVER_ID);
             END LOOP;
             
-            -- Close the reserved cursor
             CLOSE RESERVED_CURSOR;
             
         ELSE
@@ -303,8 +308,7 @@ CREATE OR REPLACE PACKAGE BODY STAFFPACKAGE AS
         SELECT COUNT(*) INTO RESERVED_COUNT FROM RESERVED_BOOKS WHERE BOOK_ID = NOTIFYUSER.BOOK_ID;
         
         IF RESERVED_COUNT <> 0 THEN
-            -- THIS IS GOING TO SELECT THE USER WHO HAS THE PRIORITY OF BORROWING THE BOOK IMMEDIATELY
-            DBMS_OUTPUT.PUT_LINE('I GOT HERE 1');
+            -- This is going to select the user who has the priority of borrowing the book 
             SELECT RESERVER_ID INTO NEXT_PRIORITY_ID
                     FROM (
                         SELECT RESERVER_ID, R_BOOK_ID
@@ -313,11 +317,8 @@ CREATE OR REPLACE PACKAGE BODY STAFFPACKAGE AS
                         ORDER BY R_BOOK_ID
                         FETCH FIRST 1 ROW ONLY);
             
-            DBMS_OUTPUT.PUT_LINE('I GOT HERE 2');
             SELECT USER_NAME INTO PRIORITY_NAME FROM USERS WHERE USERS.USER_ID = NEXT_PRIORITY_ID;
-            DBMS_OUTPUT.PUT_LINE('I GOT HERE 3');
             SELECT BOOK_NAME INTO BOOK_NAME FROM LIBRARY_BOOKS WHERE BOOK_ID = NOTIFYUSER.BOOK_ID;
-            DBMS_OUTPUT.PUT_LINE('I GOT HERE 4');
             
             DBMS_OUTPUT.PUT_LINE('Mr./Ms. ' || PRIORITY_NAME || ', your reserved book ' || BOOK_NAME || ' is available for borrowing.');
         END IF;
@@ -328,17 +329,17 @@ END STAFFPACKAGE;
 
 -- ADD BOOK
 BEGIN
-    STAFFPACKAGE.ADDBOOK('Java Programming', 'D15R40', 1);
+    STAFFPACKAGE.ADDBOOK('PLSQL Programming', 'A12R10', 3);
 END;
 
 -- DELETE BOOK
 BEGIN
-    STAFFPACKAGE.DELETEBOOK(1055, 3);
+    STAFFPACKAGE.DELETEBOOK(1056, 26);
 END;
 
 -- GENERATE REPORT
 BEGIN
-    STAFFPACKAGE.GENERATEREPORT('RESERVED');
+    STAFFPACKAGE.GENERATEREPORT('BORROWED');
 END;
 
 
